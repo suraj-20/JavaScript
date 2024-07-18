b0728ec4f5abbb4e86fcee5f0a222e54c7e4da00
Suraj <kumarsuraj54057@gmail.com>
2024-07-18 11:01:46 +0530

    first commit

---
+++ one.js
@@ -0,0 +1,11 @@
+// Javascript is a synchronous single-threaded langauage.
+
+// Synchronous single-threaded language means that JavaScript can only execute one command at a time , and in a specific order.
+
+// Everything in JavaScript happens inside a execution context.
+
+// Execution context have two components i.e., Memory or Code.
+
+// Memory stores the key: value payers, or functions() in it, kay: value means that a = 10, and a functions means that the whole function: fn(){..}
+
+// Code execute the commands one by one to get the desired output.
