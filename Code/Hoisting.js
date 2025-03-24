c94d870f07ef4f3808f975a8e52b1d561325e95a
Suraj <kumarsuraj54057@gmail.com>
2025-03-24 13:45:19 +0530

    hoisting and let-var-const

--- Code/Hoisting.js
+++ Code/Hoisting.js
@@ -7,3 +7,18 @@ var message = "This variable has been hoisted";
 function getName(name) {
   console.log(name);
 }
+
+function sayMyName(characterName) {
+  var profession = 'Drug Dealer';
+
+  if (characterName) {
+    let age = 50;
+    console.log(profession); // Drug Dealer
+    console.log(age); // 50
+  }
+
+  console.log(age); // ReferenceError: age is not defined
+  console.log(noOfChildren); // ReferenceError: noOfChildren is not defined
+}
+
+sayMyName('Walter White');
--- Code/let-var-const.js
+++ Code/let-var-const.js
@@ -4,17 +4,28 @@
 
 var a = 10;
 a = 40;
-console.log(a); // 40
+console.log("Outside function var : ",a); // 40
 
 let b = 20;
 b = 50;
-console.log(b); // 50
+console.log("Outside Function let : " ,b); // 50
 
 const c = 30;
-c = 60;
+// c = 60;
 console.log(c); // TypeError: Assignment to constant variable.
 
 // console.log(a);  //10
 // console.log(b);  //20
 // console.log(c);  //30
+
+function checkVar() {
+    var a = 60;
+    console.log("Inside function var : ",a);
+    let b = 80;
+    console.log("Inside function let : ",b);
+}
+
+checkVar();
+// console.log("After function declaration var : ",a);
+// console.log("After function declaration let : ",b);
 
